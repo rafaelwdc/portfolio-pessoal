784b44b8d3576f07c53716ee5e933d999b680618
rafaelwdc <rafaelmfreitas93@gmail.com>
2023-10-09 20:46:22 -0300

    atualizando o javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
 // Cria a Variável que irá armazenar o id do menu hamburguer ;
-const botao__menu__hamburguer = document.querySelector('#botao__menu__hamburguer');
+const menu_hamburguer = document.querySelector('#botao-menu-hamburguer');
 
 // Cria o evento que expande o menu navegaçao;
-function expandir__menu(evento) {
+function expandir_menu(evento) {
 
     // Abre uma Condição que testa se o evento vem de um aparelho mobile, para evitar duplo click; 
     if(evento.type == 'touchstart') {
         evento.preventDefault();
     }
     // Cria a variável que seleciona qual modo está o menu navegação que fica dentro do menu hamburguer; 
-    const abrir__menu__hamburguer = document.querySelector('#nav');
+    const abrir_menu_hamburguer = document.querySelector('#nav');
 
     // Expande o menu quando 'active' estiver selecionado;
-    abrir__menu__hamburguer.classList.toggle('active');
-    const ativo = abrir__menu__hamburguer.classList.contains('active');
+    abrir_menu_hamburguer.classList.toggle('active');
+    const ativo = abrir_menu_hamburguer.classList.contains('active');
     evento.currentTarget.setAttribute('aria-expanded', ativo);
 
     // Abre a condição para avisar o navegador que o menu esta aberto ou fechado, para fins de acessibilidade;
@@ -25,5 +25,5 @@ function expandir__menu(evento) {
 }
 
 // Adiciona os escutadores para os eventos de click do mouse, ou em touch em caso de aparelhos Mobile;
-botao__menu__hamburguer.addEventListener('click', expandir__menu);
-botao__menu__hamburguer.addEventListener('touchstart', expandir__menu);
+menu_hamburguer.addEventListener('click', expandir_menu);
+menu_hamburguer.addEventListener('touchstart', expandir_menu);
